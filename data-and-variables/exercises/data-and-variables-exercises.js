c4280689695202439605b172d89a6c385e735276
Kevin Collins <ghcs2005@gmail.com>
2023-09-09 16:43:55 -0500

    Days To Moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -12,21 +12,29 @@
 
 const spaceShuttle = ("Determination");
 let shuttleSpeed = (17500);
-const distanceToMars = (225000000);
+//const distanceToMars = (225000000);
 const distanceToMoon = (384400);
 const milesPerKilometer = (0.621);
-let milesToMars = (distanceToMars * milesPerKilometer);
-let hoursToMars = (milesToMars / shuttleSpeed)
-let daysToMars = (hoursToMars / 24);
+
+//let milesToMars = (distanceToMars * milesPerKilometer);
+let milesToMoon = (distanceToMoon * milesPerKilometer);
+
+//let hoursToMars = (milesToMars / shuttleSpeed);
+
+let hoursToMoon = (milesToMoon / shuttleSpeed);
+
+//let daysToMars = (hoursToMars / 24);
+
+let daysToMoon = (hoursToMoon / 24);
 
 console.log(spaceShuttle);
 console.log(shuttleSpeed);
-console.log(distanceToMars);
+//console.log(distanceToMars);
 console.log(distanceToMoon);
 console.log(milesPerKilometer);
 
-console.log(milesToMars);
-console.log (spaceShuttle + " will" + " take "  + daysToMars + " days" + " to" + " reach" + " Mars.");
+//console.log(milesToMars);
+console.log (spaceShuttle + " will" + " take "  + daysToMoon + " days" + " to" + " reach" + " the " + "Moon.");
 
 
 
